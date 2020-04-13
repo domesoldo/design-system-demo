236851c36c3094af5fdd747eff2a45ca9efc407d
Domenico Soldo (AU) <domenico.soldo@pwc.com>
2020-04-13 12:33:38 +1000

    update storybook

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from '../src';
+import { Button, ThemeProvider, Theme } from '../src';
 
 export default {
   title: 'Buttons',
@@ -10,3 +10,24 @@ export const Primary = () => <Button title="PRESS ME" />;
 export const Danger = () => <Button title="PRESS ME" color="danger" />;
 
 export const Outline = () => <Button title="PRESS ME" variant="outline" />;
+
+export const Small = () => <Button title="PRESS ME" size="s" />;
+
+export const Large = () => <Button title="PRESS ME" size="l" />;
+
+export const FullWidth = () => <Button title="PRESS ME" fullWidth={true} />;
+
+export const WithThemeProvider = () => {
+  const custonTheme: Theme = {
+    palette: {
+      light: 'white',
+      base: 'pink',
+      dark: '',
+    },
+  };
+  return (
+    <ThemeProvider theme={custonTheme}>
+      <Button title="PRESS ME" variant="outline" />
+    </ThemeProvider>
+  );
+};
